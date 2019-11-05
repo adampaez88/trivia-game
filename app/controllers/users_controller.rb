78edaa48cbb513d9587e90b73551b939a4594ca9
github username <github email address>
2019-11-05 10:53:26 -0700

    added controller functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,31 @@
 class UsersController < ApplicationController
-    
+    def index 
+        @users = User.all 
+
+        render json: @users
+    end 
+
+    def show 
+        @user = User.find(params[:id])
+
+        render json: @user
+    end 
+
+    def create 
+        @user = User.create(name: params[:name])
+
+        render json: @user
+    end 
+
+    def update 
+        @user = User.find(params[:id])
+        @user.update(name: params[:name])
+        render json: @user
+    end 
+
+    def destroy 
+        @user = User.find(params[:id])
+        @user.destroy
+        render json: {stauts: 204}
+    end 
 end
