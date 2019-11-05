3367c19000fcd574bfd62ad9712697fab776cb43
github username <github email address>
2019-11-05 11:51:12 -0700

    added seeds and api

--- db/migrate/20191105171209_create_questions.rb
+++ db/migrate/20191105171209_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[6.0]
   def change
     create_table :questions do |t|
       t.string :category
-      t.string :type
+      t.string :question_type 
       t.string :difficulty
       t.string :question
       t.string :correct_answer
